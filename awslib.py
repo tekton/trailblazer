62ccae9df572512b87925e5bf0915f966d98a3e3
tekkton <tyler@pyroturtle.com>
2019-06-17 09:59:04 -0700

    init skeletons

---
+++ awslib.py
@@ -0,0 +1,18 @@
+import boto3
+
+"""
+	To keep all the AWS interactions together that aren't the base
+	handlers, we'll shove it all in here just in case.
+"""
+
+
+def get_session():
+	pass
+
+
+def get_from_dynamo():
+	pass
+
+
+def push_to_dynamo():
+	pass
---
+++ lambda.py
@@ -0,0 +1,10 @@
+import boto3
+import hasher
+
+
+def push_handler(event, context):
+	pass
+
+
+def get_handler(event, context):
+	pass
